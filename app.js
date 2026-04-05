b398128f531f226d59885acb676cd6085e130754
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-04-05 11:39:53 +0530

    note model

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require("express")
 
 const app = express()
+const noteModel = require("./models/note.model")
+
+app.use(express.json())
+
+//post /api/notes
+
+app.post('/api/notes', async (req, res)=>{
+    const {title, description} = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message: "note created successfully",
+        note
+
+    })
+})
+
+
 
 module.exports = app
 
 
-//server create
