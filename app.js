afbc4098fde8a39f20ef641e4d25d9bab257f2d5
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-04-05 20:26:15 +0530

    get api notes fetced sucessfully

--- app.js
+++ app.js
@@ -10,18 +10,25 @@ app.use(express.json())
 app.post('/api/notes', async (req, res)=>{
     const {title, description} = req.body
 
-    const note = await noteModel.create({
+    const notes = await noteModel.create({
         title, description
     })
 
     res.status(201).json({
         message: "note created successfully",
-        note
+        notes
 
     })
 })
 
+app.get("/api/notes", async (req, res)=>{
+    const notes = await noteModel.find()
 
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes
+    })
+})
 
 module.exports = app
 
